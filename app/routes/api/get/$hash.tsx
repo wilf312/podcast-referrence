563b6c427884c44c9762c63a2e0cc62c2478848c
okada genya <g.okada.wilf+github@gmail.com>
2022-08-26 09:58:54 +0900

    add: podcast item api

---
+++ app/routes/api/get/$hash.tsx
@@ -0,0 +1,38 @@
+/**
+ * API
+ */
+import type { LoaderFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+
+import { getEncodedUrl } from "~/config";
+
+const getJSON = async (hashEncoded: string) => {
+  const url = `https://action-ten.vercel.app/${hashEncoded}.json`;
+  console.log({ url });
+  const res = await fetch(url, {
+    method: "GET",
+    mode: "cors",
+  }).then((res) => {
+    return res.json();
+  });
+  return res;
+};
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const a = getEncodedUrl();
+
+  const found = a.find((data) => {
+    return data.hash === params.hash;
+  });
+
+  if (!found) {
+    throw new Response("Content Not Found.", {
+      status: 404,
+    });
+  }
+  console.log({ params, found });
+
+  const res = await getJSON(found?.hashEncoded);
+
+  return json(res);
+};
