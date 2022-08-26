1a34b9612ce27dc6c90e9348312d6f35d1e20092
okada genya <g.okada.wilf+github@gmail.com>
2022-08-26 16:17:39 +0900

    fix: tweet url in episode page

--- app/routes/content/$podcastName/$episode.tsx
+++ app/routes/content/$podcastName/$episode.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from "react";
 import { json } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/cloudflare";
 import { EpisodeList } from "~/components/stateless/EpisodeList";
 import type { EpisodeItem } from "~/components/stateless/EpisodeList";
 import { Player } from "~/components/stateless/Player";
+import { findPodcastConfig } from "~/config";
 
 export const loader: LoaderFunction = async ({ params, request }) => {
   const url = new URL(request.url);
@@ -11,7 +13,11 @@ export const loader: LoaderFunction = async ({ params, request }) => {
     `${url.origin}/api/get/${params.podcastName}`
   ).then((res) => res.json());
 
+  const config = findPodcastConfig(params.podcastName || "");
+
   return json({
+    href: request.url,
+    config,
     params,
     podcast,
   });
@@ -20,6 +26,7 @@ export default function Index() {
   const data = useLoaderData<typeof loader>();
 
   console.log(data);
+
   const episodeList = data.podcast.item;
   const episode = episodeList.find((d: EpisodeItem) => {
     return (
@@ -28,15 +35,24 @@ export default function Index() {
     );
   });
 
-  const text = `
-`;
-  const tweetLink = `https://twitter.com/intent/tweet?text=${text}`;
+  const tweetLink = useMemo(() => {
+    let search = "";
+    if (global.location) {
+      search = location.search;
+    }
+    const text = encodeURIComponent(
+      `${data.href}${search} #${data?.config?.hash}`
+    );
+    return `https://twitter.com/intent/tweet?text=${text}`;
+  }, []);
 
   return (
     <div>
       <h1>{data?.podcast?.title}</h1>
 
-      {episode && <Player src={episode.enclosure["@url"]} />}
+      {episode && (
+        <Player src={episode.enclosure["@url"]} tweetUrl={tweetLink} />
+      )}
       <EpisodeList
         episodeList={episodeList}
         podcastName={data.params.podcastName}
