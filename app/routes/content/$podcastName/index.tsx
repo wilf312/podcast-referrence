76aec5362c0ef6fef86f1eb3b725ca30272fe05a
okada genya <g.okada.wilf+github@gmail.com>
2022-08-28 20:41:33 +0900

    ポッドキャストのルートページアクセス時に最新回へリダイレクトするようにした。（仕様が複雑化しそうだったのでルートのコンテンツが本当に必要になった時に考える

--- app/routes/content/$podcastName/index.tsx
+++ app/routes/content/$podcastName/index.tsx
@@ -1,55 +1,18 @@
-import { json } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { redirect } from "@remix-run/cloudflare";
 import type { LoaderFunction } from "@remix-run/cloudflare";
-import { EpisodeList } from "~/components/stateless/EpisodeList";
-import { Player } from "~/components/stateless/Player";
-import { findPodcastConfig } from "~/config";
 import { getPodcast } from "~/api";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  const podcast = await getPodcast(params.podcastName || "");
-  const config = findPodcastConfig(params.podcastName || "");
+  const podcast: any = await getPodcast(params.podcastName || "");
 
-  return json({
-    config,
-    params,
-    podcast,
-  });
-};
-export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  if (!podcast || !podcast?.item[0]) {
+    return new Response(`ページが見つかりません`, { status: 404 });
+  }
 
-  console.log({ data });
-
-  const episodeList = data.podcast.item;
-  const episode = episodeList.at(0);
-
-  const text = encodeURIComponent(`${data.href} ${data?.config?.hash}`);
-  const tweetLink = `https://twitter.com/intent/tweet?text=${text}`;
-  return (
-    <div>
-      <h1>{data?.podcast?.title}</h1>
-
-      {episode && (
-        <Player
-          src={episode.enclosure["@url"]}
-          tweetUrl={tweetLink}
-          onPlay={() => {
-            console.log("onPlay");
-
-            const url = `${location.pathname}${
-              episode["itunes:episode"] ||
-              encodeURIComponent(episode.guid["#text"])
-            }`;
-            // https://developer.mozilla.org/ja/docs/Web/API/History/replaceState
-            history.replaceState(null, "", url);
-          }}
-        />
-      )}
-      <EpisodeList
-        episodeList={episodeList}
-        podcastName={data.params.podcastName}
-      />
-    </div>
+  const newestEpisode = podcast.item[0];
+  return redirect(
+    `/content/${params.podcastName}/${encodeURIComponent(
+      newestEpisode.guid["#text"]
+    )}`
   );
-}
+};
