bd792cdd76bb3965f8a2f99a417969866074344c
okada genya <g.okada.wilf+github@gmail.com>
2022-08-30 19:48:56 +0900

    日本語ハッシュタグのポッドキャストでエラーになる問題の解消

--- app/routes/content/$podcastName/index.tsx
+++ app/routes/content/$podcastName/index.tsx
@@ -5,14 +5,15 @@ import { getPodcast } from "~/api";
 export const loader: LoaderFunction = async ({ params }) => {
   const podcast: any = await getPodcast(params.podcastName || "");
 
-  if (!podcast || !podcast?.item[0]) {
+  if (!podcast || !podcast?.item[0] || !params.podcastName) {
     return new Response(`ページが見つかりません`, { status: 404 });
   }
 
   const newestEpisode = podcast.item[0];
-  return redirect(
-    `/content/${params.podcastName}/${encodeURIComponent(
-      newestEpisode.guid["#text"]
-    )}`
-  );
+
+  const redirectURL = `/content/${encodeURIComponent(
+    params.podcastName
+  )}/${encodeURIComponent(newestEpisode.guid["#text"])}`;
+  console.log({ length: redirectURL.length });
+  return redirect(redirectURL);
 };
