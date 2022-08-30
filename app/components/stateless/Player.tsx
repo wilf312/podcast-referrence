e7a72ea97e75c07e22623b44b7a19eee532375d8
okada genya <g.okada.wilf+github@gmail.com>
2022-08-30 19:50:20 +0900

    タイトルが入るように修正

--- app/components/stateless/Player.tsx
+++ app/components/stateless/Player.tsx
@@ -3,18 +3,25 @@ import { usePlayer, useTweetLink } from "~/domain/player";
 type props = {
   src: string;
   hash: string;
+  title?: string;
   episodeNo: number;
 };
 export const Player = (props: props) => {
   const player = usePlayer();
   const tweetLink = useTweetLink({
     hash: props.hash,
+    title: props.title,
     episodeNo: props.episodeNo,
     currentTime: player.currentTime,
   });
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
       <audio
         controls
         src={props.src}
