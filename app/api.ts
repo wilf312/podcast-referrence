b874e91008490bcdae25fc954bedfc28bb3d3dd0
okada genya <g.okada.wilf+github@gmail.com>
2022-08-30 19:49:14 +0900

    コメントの修正

--- app/api.ts
+++ app/api.ts
@@ -1,15 +1,15 @@
 /**
  * podcast のデータの取得API
- * @param hashEncoded 
- * @returns 
+ * @param hashEncoded
+ * @returns
  */
 export const getPodcast = async (hashEncoded: string) => {
   const url = `https://action-ten.vercel.app/${hashEncoded}.json`;
+
   const res = await fetch(url, {
     method: "GET",
   }).then((res) => {
     return res.json();
   });
-  console.warn({ res });
   return res;
 };
