c5d3d00877873261ff5c5e236fbd1409f833ba03
okada genya <g.okada.wilf+github@gmail.com>
2022-09-01 08:41:54 +0900

    ログ出力するように修正

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createEventHandler } from "@remix-run/cloudflare-workers";
 import * as build from "@remix-run/dev/server-build";
 
-addEventListener(
-  "fetch",
-  createEventHandler({ build, mode: process.env.NODE_ENV })
-);
+addEventListener("fetch", (event) => {
+  console.log(event.request.headers.get("user-agent"));
+  createEventHandler({ build, mode: process.env.NODE_ENV })(event);
+});
